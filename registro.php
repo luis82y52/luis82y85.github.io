b997bc20bc5a8ba397829824f97e9c3852478177
wichi <152202203+luis82y52@users.noreply.github.com>
2023-11-27 22:44:50 -0600

    commit initial

---
+++ registro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registro de Usuario</title>
+</head>
+<center><body background="https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2023/01/Marin-Kitagawa-vuelve-a-la-playa-con-fogoso-cosplay-de-My-Dress-Up-Darling-2.jpg"></center>
+    <center><h1>Formulario de Registro</h1></center>
+
+    <center><form action="file:///C:/animelock/for.html" method="POST">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" required><br>
+
+        <label for="apellido">Apellido:</label>
+        <input type="text" id="apellido" name="apellido" required><br>
+
+        <label for="correo">Correo Electrónico:</label>
+        <input type="email" id="correo" name="correo" required><br>
+
+        <label for="contrasena">Contraseña:</label>
+        <input type="password" id="contrasena" name="contrasena" required><br>
+
+        <input type="submit" value="Registrarse"></center>
+    </form>
+</body>
+</html>
